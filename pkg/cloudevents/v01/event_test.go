8503fbb4f5aa9d8f6251dc4deda8c2a16d33f455
Scott Nichols <nicholss@google.com>
2019-02-07 18:53:47 -0800

    adding hack script to run go test.
    Signed-off-by: Scott Nichols <nicholss@google.com>

--- pkg/cloudevents/v01/event_test.go
+++ pkg/cloudevents/v01/event_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
 	"github.com/cloudevents/sdk-go/pkg/cloudevents/v01"
-	"github.com/cloudevents/sdk-go/v01"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
