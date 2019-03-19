7f108f0ca4706f76a5367ffa33a278a76a74e8df
Scott Nichols <nicholss@google.com>
2019-03-19 10:22:21 -0700

    Document context and codec.
    Signed-off-by: Scott Nichols <nicholss@google.com>

--- pkg/cloudevents/datacodec/codec.go
+++ pkg/cloudevents/datacodec/codec.go
@@ -8,7 +8,13 @@ import (
 	"github.com/cloudevents/sdk-go/pkg/cloudevents/observability"
 )
 
+// Decoder is the expected function signature for decoding `in` to `out`. What
+// `in` is could be decoder dependent. For example, `in` could be bytes, or a
+// base64 string.
 type Decoder func(in, out interface{}) error
+
+// Encoder is the expected function signature for encoding `in` to bytes.
+// Returns an error if the encoder has an issue encoding `in`.
 type Encoder func(in interface{}) ([]byte, error)
 
 var decoder map[string]Decoder
@@ -27,14 +33,21 @@ func init() {
 	AddEncoder("application/xml", xml.Encode)
 }
 
+// AddDecoder registers a decoder for a given content type. The codecs will use
+// these to decode the data payload from a cloudevent.Event object.
 func AddDecoder(contentType string, fn Decoder) {
 	decoder[contentType] = fn
 }
 
+// AddEncoder registers an encoder for a given content type. The codecs will
+// use these to encode the data payload for a cloudevent.Event object.
 func AddEncoder(contentType string, fn Encoder) {
 	encoder[contentType] = fn
 }
 
+// Decode looks up and invokes the decoder registered for the given content
+// type. An error is returned if no decoder is registered for the given
+// content type.
 func Decode(contentType string, in, out interface{}) error {
 	// TODO: wire in context.
 	_, r := observability.NewReporter(context.Background(), ReportDecode)
@@ -54,6 +67,9 @@ func obsDecode(contentType string, in, out interface{}) error {
 	return fmt.Errorf("[decode] unsupported content type: %q", contentType)
 }
 
+// Encode looks up and invokes the encoder registered for the given content
+// type. An error is returned if no encoder is registered for the given
+// content type.
 func Encode(contentType string, in interface{}) ([]byte, error) {
 	// TODO: wire in context.
 	_, r := observability.NewReporter(context.Background(), ReportEncode)
