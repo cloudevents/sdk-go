78f3863c8826373445f73643fc354dc7f4287ecb
Scott Nichols <nicholss@google.com>
2019-05-29 12:53:34 -0700

    Adding test for conversions.
    Signed-off-by: Scott Nichols <nicholss@google.com>

---
+++ test/http/conversion_v02_test.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"github.com/cloudevents/sdk-go"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestClientConversion_v02(t *testing.T) {
+	now := time.Now()
+
+	testCases := ConversionTestCases{
+		"Conversion v0.2": {
+			now:       now,
+			convertFn: UnitTestConvert,
+			data:      map[string]string{"hello": "unittest"},
+			want: &cloudevents.Event{
+				Context: cloudevents.EventContextV02{
+					ID:     "321-CBA",
+					Type:   "io.cloudevents.conversion.http.post",
+					Source: *cloudevents.ParseURLRef("github.com/cloudevents/test/http/conversion"),
+				}.AsV02(),
+				Data: map[string]string{"unittest": "response"},
+			},
+			asSent: &TapValidation{
+				Method: "POST",
+				URI:    "/",
+				Header: map[string][]string{
+					"content-type": {"application/json"},
+				},
+				Body:          `{"hello":"unittest"}`,
+				ContentLength: 20,
+			},
+			asRecv: &TapValidation{
+				Header:        http.Header{},
+				Status:        "202 Accepted",
+				ContentLength: 0,
+			},
+		},
+	}
+
+	for n, tc := range testCases {
+		t.Run(n, func(t *testing.T) {
+			ClientConversion(t, tc)
+		})
+	}
+}
