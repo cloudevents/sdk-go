9caf28f796682829175d0c2c55b26127085f6e8d
R.I.Pienaar <rip@devco.net>
2019-12-09 19:19:38 +0100

    Improve NATS transport flexibility (#268) (#269)
    * Improve NATS transport flexibility (#268)
    
    This allow users to pass NATS connection options to the transport
    which would allow them to set up custom authentication, logging and
    other core nats settings
    
    * Add tests for WithConnOptions (#268)
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- pkg/cloudevents/transport/nats/options.go
+++ pkg/cloudevents/transport/nats/options.go
@@ -1,5 +1,9 @@
 package nats
 
+import (
+	"github.com/nats-io/nats.go"
+)
+
 // Option is the function signature required to be considered an nats.Option.
 type Option func(*Transport) error
 
@@ -7,6 +11,18 @@ type Option func(*Transport) error
 func WithEncoding(encoding Encoding) Option {
 	return func(t *Transport) error {
 		t.Encoding = encoding
+		return nil
+	}
+}
+
+// WithConnOptions supplies NATS connection options that will be used when setting
+// up the internal NATS connection
+func WithConnOptions(opts ...nats.Option) Option {
+	return func(t *Transport) error {
+		for _, o := range opts {
+			t.ConnOptions = append(t.ConnOptions, o)
+		}
+
 		return nil
 	}
 }
--- pkg/cloudevents/transport/nats/options_test.go
+++ pkg/cloudevents/transport/nats/options_test.go
@@ -2,9 +2,12 @@ package nats
 
 import (
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	natsd "github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
 )
 
 func TestWithEncoding(t *testing.T) {
@@ -45,5 +48,36 @@ func TestWithEncoding(t *testing.T) {
 				t.Errorf("unexpected (-want, +got) = %v", diff)
 			}
 		})
+	}
+}
+
+func TestWithConnOptions(t *testing.T) {
+	opts := []nats.Option{
+		nats.DontRandomize(),
+		nats.NoEcho(),
+	}
+
+	srv, err := natsd.NewServer(&natsd.Options{Port: -1})
+	if err != nil {
+		t.Errorf("could not start nats server: %w", err)
+	}
+	go srv.Start()
+	defer srv.Shutdown()
+
+	if !srv.ReadyForConnections(10 * time.Second) {
+		t.Errorf("nats server did not start")
+	}
+
+	tr, err := New(srv.Addr().String(), "testing", WithConnOptions(opts...))
+	if err != nil {
+		t.Errorf("connection failed: %w", err)
+	}
+
+	if !tr.Conn.Opts.NoRandomize {
+		t.Errorf("NoRandomize option was not set")
+	}
+
+	if !tr.Conn.Opts.NoEcho {
+		t.Errorf("NoEcho option was not set")
 	}
 }
