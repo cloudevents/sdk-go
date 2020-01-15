415a945609cc30c7ab806420137ef9fe9260398f
Alan Conway <aconway@redhat.com>
2020-01-15 17:23:35 -0500

    Restore StructMessage, complete EventMessage implementation.
    StructMessage and EventMessage are intended to be full implementations of the
    Message interface that are independent of any transport and can be used to hold
    a copy of a message in memory independently of the life of an incoming transport
    message (e.g. for multicast, queuing etc.) CopyMessage copies an incoming
    Message, the copy can be read many times and is independent of the original
    message lifecycle (e.g. you might Finish() the original before you're done
    multicasting the copy depending on policy)
    
    They are a lot like your MockXMessages but they are not mocks, they're complete
    Message implementations. Possibly there's some code overlap that can be cleaned up.
    
    Please review the code carefully. I like what you've done to reduce copying but
    there was a lot to take in, so this is a bit rushed.
    
    One question: I intended for 2 types of message representation - Binary and
    Structured. Message.Event() and EventMessage were really just implementations of
    Binary encoding - not meant to be a third representation. I think you could drop
    Message.Event() now that you have ToEvent(Message) so bindings only have to
    implement the methods Structured() and Binary().
    
    Signed-off-by: Alan Conway <aconway@redhat.com>

--- pkg/binding/event_message.go
+++ pkg/binding/event_message.go
@@ -1,15 +1,71 @@
 package binding
 
-import ce "github.com/cloudevents/sdk-go/pkg/cloudevents"
+import (
+	"bytes"
+
+	"github.com/cloudevents/sdk-go/pkg/binding/spec"
+	ce "github.com/cloudevents/sdk-go/pkg/cloudevents"
+)
+
+var specs = spec.New()
 
 // EventMessage type-converts a cloudevents.Event object to implement Message.
 // This allows local cloudevents.Event objects to be sent directly via Sender.Send()
 //     s.Send(ctx, binding.EventMessage(e))
 type EventMessage ce.Event
 
-func (m EventMessage) Event(builder EventEncoder) error   { return builder.SetEvent(ce.Event(m)) }
-func (m EventMessage) Structured(StructuredEncoder) error { return ErrNotStructured }
-func (m EventMessage) Binary(BinaryEncoder) error         { return ErrNotBinary }
-func (EventMessage) Finish(error) error                   { return nil }
+func (m EventMessage) Event(builder EventEncoder) error {
+	return builder.SetEvent(ce.Event(m))
+}
+
+func (m EventMessage) Structured(StructuredEncoder) error {
+	return ErrNotStructured
+}
+
+func (m EventMessage) Binary(b BinaryEncoder) (err error) {
+	c := m.Context
+	var sv spec.Version
+	sv, err = specs.Version(c.GetSpecVersion())
+	if err != nil {
+		return err
+	}
+	set := func(k spec.Kind, v interface{}) {
+		if err == nil {
+			attr := sv.AttributeFromKind(k)
+			if attr != nil {
+				err = b.SetAttribute(attr, v)
+			}
+		}
+	}
+	set(spec.SpecVersion, c.GetSpecVersion())
+	set(spec.Type, c.GetType())
+	set(spec.Source, c.GetSource())
+	set(spec.Subject, c.GetSubject())
+	set(spec.ID, c.GetID())
+	set(spec.Time, c.GetTime())
+	set(spec.DataSchema, c.GetDataSchema())
+	set(spec.DataContentType, c.GetDataContentType())
+
+	for k, v := range c.GetExtensions() {
+		if err == nil {
+			err = b.SetExtension(k, v)
+		}
+	}
+	if err != nil {
+		return err
+	}
+	body, err := (*ce.Event)(&m).DataBytes()
+	if err == nil && len(body) > 0 {
+		return b.SetData(bytes.NewReader(body))
+	}
+	return err
+}
+
+func (EventMessage) Finish(error) error { return nil }
+
+func (m *EventMessage) SetEvent(e ce.Event) error {
+	*m = EventMessage(e)
+	return nil
+}
 
 var _ Message = (*EventMessage)(nil) // Test it conforms to the interface
