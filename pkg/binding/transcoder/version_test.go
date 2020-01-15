a1f233da22d9297e92df46d4543aa9abbb57756c
Alan Conway <aconway@redhat.com>
2020-01-15 15:35:13 -0500

    Fix scope warnings from golangci-lint
    Signed-off-by: Alan Conway <aconway@redhat.com>

--- pkg/binding/transcoder/version_test.go
+++ pkg/binding/transcoder/version_test.go
@@ -59,6 +59,7 @@ func TestVersionTranscoder(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt // Don't use range variable inside scope
 		factory := Version(spec.V1)
 		t.Run(tt.name, func(t *testing.T) {
 			e, _, _, err := binding.ToEvent(tt.message, factory)
