e901d1bfea634565571714d4491e77a0dac3e7e8
Francesco Guardiani <francescoguard@gmail.com>
2020-02-20 19:20:50 +0100

    Added AMQP Router to CircleCI (#308)
    * Added AMQP Router to CircleCI
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Fixed bad usage of asserts
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Fixed issues with AMQP encoding
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Triggering the CI
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Added test url
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- hack/presubmit-test.sh
+++ hack/presubmit-test.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 # Test everything in pkg and cmd
-go test ./pkg/... ./cmd/... -coverprofile cover.out -timeout 15s
+go test -v ./pkg/... ./cmd/... -coverprofile cover.out -timeout 15s
 
 # Test everything in test with a slightly longer timeout
 go test ./test/... -timeout 60s
--- pkg/bindings/amqp/receiver.go
+++ pkg/bindings/amqp/receiver.go
@@ -12,7 +12,11 @@ type Receiver struct{ AMQP *amqp.Receiver }
 
 func (r *Receiver) Receive(ctx context.Context) (binding.Message, error) {
 	m, err := r.AMQP.Receive(ctx)
-	return NewMessage(m), err
+	if err != nil {
+		return nil, err
+	}
+
+	return NewMessage(m), nil
 }
 
 func (r *Receiver) Close(ctx context.Context) error { return r.AMQP.Close(ctx) }
