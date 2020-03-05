ab1567ff5f2e0d08bdf3873c4c26c2e4b18540c4
Scott Nichols <snichols@vmware.com>
2020-03-05 15:17:51 -0800

    start implementing httpb transport
    Signed-off-by: Scott Nichols <snichols@vmware.com>

---
+++ cmd/samples/httpb/receiver/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/cloudevents/sdk-go/pkg/cloudevents/transport/httpb"
+	"log"
+
+	"github.com/cloudevents/sdk-go"
+)
+
+func main() {
+	ctx := context.Background()
+
+	t, err := httpb.New()
+	if err != nil {
+		log.Fatalf("failed to create transport, %v", err)
+	}
+
+	c, err := cloudevents.NewClient(t)
+	if err != nil {
+		log.Fatalf("failed to create client, %v", err)
+	}
+
+	log.Printf("will listen on :8080\n")
+	log.Fatalf("failed to start receiver: %s", c.StartReceiver(ctx, receive))
+}
+
+func receive(ctx context.Context, event cloudevents.Event) {
+	fmt.Printf("%s", event)
+}
