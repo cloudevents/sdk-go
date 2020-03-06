8e62d37f28f0eb8435407110866ad0eaa47b8a77
slinkydeveloper <francescoguard@gmail.com>
2020-03-06 09:32:58 +0100

    Now EventMessage can be plugged to use different marshalling formats for Structured mode
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ pkg/binding/event_message_test.go
@@ -0,0 +1,44 @@
+package binding_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cloudevents/sdk-go/pkg/binding"
+	"github.com/cloudevents/sdk-go/pkg/binding/test"
+	"github.com/cloudevents/sdk-go/pkg/event"
+)
+
+type mockFormat struct {
+	t             *testing.T
+	expectedEvent event.Event
+}
+
+func (m *mockFormat) MediaType() string {
+	return "application/cool-mediatype"
+}
+
+func (m *mockFormat) Marshal(have event.Event) ([]byte, error) {
+	test.AssertEventEquals(m.t, m.expectedEvent, have)
+	return []byte{}, nil
+}
+
+func (m *mockFormat) Unmarshal([]byte, *event.Event) error {
+	m.t.Fatal("This should never be invoked")
+	return nil
+}
+
+func TestStructuredModeCustomFormat(t *testing.T) {
+	e := test.FullEvent()
+	format := mockFormat{t: t, expectedEvent: e}
+
+	ctx := binding.UseFormatForEvent(context.TODO(), &format)
+	enc := test.MockStructuredMessage{}
+	message := binding.EventMessage(e)
+
+	err := message.Structured(ctx, &enc)
+	require.NoError(t, err)
+	require.Equal(t, &format, enc.Format)
+}
