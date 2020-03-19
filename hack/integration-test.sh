18a49d547ba09c7f51c44ba16b48e8c0b434a62d
Francesco Guardiani <francescoguard@gmail.com>
2020-03-19 14:54:27 +0100

    Fixed code coverage script (#402)
    * Fixed code coverage script
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- hack/integration-test.sh
+++ hack/integration-test.sh
@@ -8,7 +8,18 @@ set -o pipefail
 pushd ./v2
 
 # Run integration tests not in parallel
-go test -v -parallel 1 ./test/... -coverprofile ${TEST_RESULTS:-.}/integration_test_cover.out -timeout 60s
+# Prepare coverage file only if not exists
+if [ ! -f ./coverage.txt ]; then
+  touch ./coverage.tmp
+  echo 'mode: atomic' > ./coverage.txt
+fi
+COVERPKG=$(go list ./... | grep -v /vendor | tr "\n" ",")
+for gomodule in $(go list ./test/... | grep -v /cmd | grep -v /vendor)
+do
+  go test -v -parallel 1 -timeout 60s -covermode=atomic -coverprofile=coverage.tmp -coverpkg "$COVERPKG" "$gomodule" 2>&1 | sed 's/ of statements in.*//; /warning: no packages being tested depend on matches for pattern /d'
+  tail -n +2 coverage.tmp >> ./coverage.txt
+done
+rm coverage.tmp
 
 # Remove test only deps.
 go mod tidy
