2efc504d873b80a06fc3cf3dad46ef41ed975d14
slinkydeveloper <francescoguard@gmail.com>
2020-03-19 11:16:27 +0100

    Fixed overwrite in integration test
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- hack/integration-test.sh
+++ hack/integration-test.sh
@@ -8,10 +8,13 @@ set -o pipefail
 pushd ./v2
 
 # Run integration tests not in parallel
-touch ./coverage.tmp
-echo 'mode: atomic' > ./coverage.txt
+# Prepare coverage file only if not exists
+if [ ! -f /tmp/foo.txt ]; then
+  touch ./coverage.tmp
+  echo 'mode: atomic' > ./coverage.txt
+fi
 COVERPKG=$(go list ./... | grep -v /vendor | tr "\n" ",")
-for gomodule in $(go list ./test/.... | grep -v /cmd | grep -v /vendor)
+for gomodule in $(go list ./test/... | grep -v /cmd | grep -v /vendor)
 do
   go test -v -parallel 1 -timeout 60s -covermode=atomic -coverprofile=coverage.tmp -coverpkg "$COVERPKG" "$gomodule" 2>&1 | sed 's/ of statements in.*//; /warning: no packages being tested depend on matches for pattern /d'
   tail -n +2 coverage.tmp >> ./coverage.txt
