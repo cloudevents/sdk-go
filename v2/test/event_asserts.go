5a785456261df2fcaa92d7399d1fe2b872c50349
slinkydeveloper <francescoguard@gmail.com>
2020-05-27 16:11:07 +0200

    Created some nicer assertions for events
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- v2/test/event_asserts.go
+++ v2/test/event_asserts.go
@@ -10,6 +10,111 @@ import (
 	"github.com/cloudevents/sdk-go/v2/event"
 )
 
+type EventMatcher func(t testing.TB, have event.Event)
+
+// ContainsContextAttributes checks if the event contains at least the provided context attributes
+func ContainsContextAttributes(attrs ...string) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		haveVersion := spec.VS.Version(have.SpecVersion())
+		for _, k := range attrs {
+			attr := haveVersion.Attribute(k)
+			require.NotNil(t, attr, "Attribute name '%s' unrecognized", k)
+			require.NotNil(t, attr.Get(have.Context))
+		}
+	}
+}
+
+// HasContextAttributes checks if the event contains at least the provided context attributes and their values
+func HasContextAttributes(m map[string]interface{}) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		haveVersion := spec.VS.Version(have.SpecVersion())
+		for k, v := range m {
+			attr := haveVersion.Attribute(k)
+			require.NotNil(t, attr, "Attribute name '%s' unrecognized", k)
+			require.Equal(t, v, attr.Get(have.Context))
+		}
+	}
+}
+
+// ContainsExtensions checks if the event contains at least the provided extension names
+func ContainsExtensions(exts ...string) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		for _, ext := range exts {
+			require.NotNil(t, have.Extensions()[ext], "Expecting extension %s not to be nil", ext)
+		}
+	}
+}
+
+// HasExactlyExtensions checks if the event contains exactly the provided extensions
+func HasExactlyExtensions(ext map[string]interface{}) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		require.Equal(t, ext, have.Extensions())
+	}
+}
+
+// HasExtensions checks if the event contains at least the provided extensions
+func HasExtensions(ext map[string]interface{}) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		for k, v := range ext {
+			require.Equal(t, v, have.Extensions()[k])
+		}
+	}
+}
+
+// HasExtension checks if the event contains the provided extension
+func HasExtension(key string, value interface{}) EventMatcher {
+	return HasExtensions(map[string]interface{}{key: value})
+}
+
+// HasData checks if the event contains the provided data
+func HasData(want []byte) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		require.Equal(t, want, have.Data())
+	}
+}
+
+// HasData checks if the event doesn't contain data
+func HasNoData() EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		require.Nil(t, have.Data())
+	}
+}
+
+// IsEqualTo performs a semantic equality check of the event (like AssertEventEquals)
+func IsEqualTo(want event.Event) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		AssertEventEquals(t, want, have)
+	}
+}
+
+// IsContextEqualTo performs a semantic equality check of the event context (like AssertEventContextEquals)
+func IsContextEqualTo(want event.Event) EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		AssertEventContextEquals(t, want.Context, have.Context)
+	}
+}
+
+// IsValid checks if the event is valid
+func IsValid() EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		require.NoError(t, have.Validate())
+	}
+}
+
+// IsValid checks if the event is invalid
+func IsInvalid() EventMatcher {
+	return func(t testing.TB, have event.Event) {
+		require.Error(t, have.Validate())
+	}
+}
+
+// AssertEvent is a "matcher like" assertion method to test the properties of an event
+func AssertEvent(t testing.TB, have event.Event, assertions ...EventMatcher) {
+	for _, a := range assertions {
+		a(t, have)
+	}
+}
+
 // AssertEventContextEquals asserts that two event.Event contexts are equals
 func AssertEventContextEquals(t testing.TB, want event.EventContext, have event.EventContext) {
 	wantVersion := spec.VS.Version(want.GetSpecVersion())
---
+++ v2/test/event_asserts_test.go
@@ -0,0 +1,51 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/cloudevents/sdk-go/v2/event"
+)
+
+func TestAssertEvent(t *testing.T) {
+	tests := []struct {
+		name       string
+		have       event.Event
+		assertions []EventMatcher
+	}{{
+		name:       "valid",
+		have:       FullEvent(),
+		assertions: []EventMatcher{IsValid()},
+	}, {
+		name:       "contains context attributes",
+		have:       FullEvent(),
+		assertions: []EventMatcher{IsValid(), ContainsContextAttributes("id", "specversion")},
+	}, {
+		name: "has context attributes",
+		have: FullEvent(),
+		assertions: []EventMatcher{IsValid(), HasContextAttributes(map[string]interface{}{
+			"id":          FullEvent().ID(),
+			"specversion": event.CloudEventsVersionV1,
+		})},
+	}, {
+		name:       "has exactly extensions",
+		have:       FullEvent(),
+		assertions: []EventMatcher{IsValid(), HasExactlyExtensions(FullEvent().Extensions())},
+	}, {
+		name:       "contains extensions",
+		have:       FullEvent(),
+		assertions: []EventMatcher{IsValid(), ContainsExtensions("exbool")},
+	}, {
+		name:       "has extension",
+		have:       FullEvent(),
+		assertions: []EventMatcher{IsValid(), HasExtension("exbool", true)},
+	}, {
+		name:       "has data",
+		have:       FullEvent(),
+		assertions: []EventMatcher{IsValid(), HasData(FullEvent().Data())},
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			AssertEvent(t, tt.have, tt.assertions...)
+		})
+	}
+}
