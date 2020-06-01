71ed2d9b2f4e6fd1aeb67b3063e6441ab1cd8c45
Cong Liu <conliu@google.com>
2020-06-01 09:36:38 -0700

    Fix pubsub message nil pointer due to missing format
    Signed-off-by: Cong Liu <conliu@google.com>

---
+++ protocol/pubsub/v2/message_test.go
@@ -0,0 +1,42 @@
+package pubsub
+
+import (
+	"context"
+	"testing"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/cloudevents/sdk-go/v2/binding"
+	"github.com/cloudevents/sdk-go/v2/event"
+)
+
+func TestReadStructured(t *testing.T) {
+	tests := []struct {
+		name    string
+		pm      *pubsub.Message
+		wantErr error
+	}{
+		{
+			name: "nil format",
+			pm: &pubsub.Message{
+				ID: "testid",
+			},
+			wantErr: binding.ErrNotStructured,
+		},
+		{
+			name: "json format",
+			pm: &pubsub.Message{
+				ID:         "testid",
+				Attributes: map[string]string{contentType: event.ApplicationCloudEventsJSON},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			msg := NewMessage(tc.pm)
+			err := msg.ReadStructured(context.Background(), (*pubsubMessagePublisher)(tc.pm))
+			if err != tc.wantErr {
+				t.Errorf("Error unexpected. got: %v, want: %v", err, tc.wantErr)
+			}
+		})
+	}
+}
