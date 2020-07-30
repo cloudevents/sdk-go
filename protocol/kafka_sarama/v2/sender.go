6a501d77d08537558594f0875c6ef7caeca0e857
Aleksander Slominski <aslom@us.ibm.com>
2020-07-30 15:14:10 -0400

    allow to construct mock sender for unit testing
    Signed-off-by: Aleksander Slominski <aslom@us.ibm.com>

--- protocol/kafka_sarama/v2/sender.go
+++ protocol/kafka_sarama/v2/sender.go
@@ -14,6 +14,7 @@ type Sender struct {
 	syncProducer sarama.SyncProducer
 }
 
+
 // NewSender returns a binding.Sender that sends messages to a specific receiverTopic using sarama.SyncProducer
 func NewSender(brokers []string, saramaConfig *sarama.Config, topic string, options ...SenderOptionFunc) (*Sender, error) {
 	// Force this setting because it's required by sarama SyncProducer
@@ -34,6 +35,11 @@ func NewSenderFromClient(client sarama.Client, topic string, options ...SenderOp
 	}
 
 	return makeSender(producer, topic, options...), nil
+}
+
+// NewSenderFromSyncProducer returns a binding.Sender that sends messages to a specific topic using sarama.SyncProducer
+func NewSenderFromSyncProducer(topic string, syncProducer sarama.SyncProducer, options ...SenderOptionFunc) (*Sender, error) {
+	return makeSender(syncProducer, topic, options...), nil
 }
 
 func makeSender(syncProducer sarama.SyncProducer, topic string, options ...SenderOptionFunc) *Sender {
