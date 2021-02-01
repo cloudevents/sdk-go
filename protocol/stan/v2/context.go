2cf43735e2054b31913627aca68412c272c48eb1
Dan <5727701+dan-j@users.noreply.github.com>
2021-02-01 13:57:23 +0000

    Implement context decorator for inbound STAN messages to expose message metadata (#658)
    Signed-off-by: dan-j <5727701+dan-j@users.noreply.github.com>
    Signed-off-by: Aneesh Puttur <aneeshputtur@gmail.com>

---
+++ protocol/stan/v2/context.go
@@ -0,0 +1,44 @@
+package stan
+
+import (
+	"context"
+	"github.com/cloudevents/sdk-go/v2/binding"
+)
+
+// MsgMetadata holds metadata of a received *stan.Msg. This information is kept in a separate struct so that users
+// cannot interact with the underlying message outside of the SDK.
+type MsgMetadata struct {
+	Sequence        uint64
+	Redelivered     bool
+	RedeliveryCount uint32
+}
+
+type msgKeyType struct{}
+
+var msgKey msgKeyType
+
+// MetadataContextDecorator returns an inbound context decorator which adds STAN message metadata to
+// the current context. If the inbound message is not a *stan.Message then this decorator is a no-op.
+func MetadataContextDecorator() func(context.Context, binding.Message) context.Context {
+	return func(ctx context.Context, m binding.Message) context.Context {
+		if msg, ok := m.(*Message); ok {
+			return context.WithValue(ctx, msgKey, MsgMetadata{
+				Sequence:        msg.Msg.Sequence,
+				Redelivered:     msg.Msg.Redelivered,
+				RedeliveryCount: msg.Msg.RedeliveryCount,
+			})
+		}
+
+		return ctx
+	}
+}
+
+// MessageMetadataFrom extracts the STAN message metadata from the provided ctx. The bool return parameter is true if
+// the metadata was set on the context, or false otherwise.
+func MessageMetadataFrom(ctx context.Context) (MsgMetadata, bool) {
+	if v, ok := ctx.Value(msgKey).(MsgMetadata); ok {
+		return v, true
+	}
+
+	return MsgMetadata{}, false
+}
