bc58a05aa98f92ae410d307449e3f843e80e3f35
Scott Nichols <snichols@vmware.com>
2021-04-06 14:24:08 -0700

    update to include observibility
    Signed-off-by: Scott Nichols <snichols@vmware.com>

--- hack/tag-release.sh
+++ hack/tag-release.sh
@@ -44,6 +44,8 @@ do
           "github.com/cloudevents/sdk-go/protocol/nats/v2"
           "github.com/cloudevents/sdk-go/protocol/pubsub/v2"
           "github.com/cloudevents/sdk-go/protocol/kafka_sarama/v2"
+          "github.com/cloudevents/sdk-go/protocol/ws/v2"
+          "github.com/cloudevents/sdk-go/observability/opencensus/v2"
           "github.com/cloudevents/sdk-go/v2"                       # NOTE: this needs to be last.
         )
         shift
@@ -104,6 +106,8 @@ MODULES=(
   "protocol/nats"
   "protocol/pubsub"
   "protocol/kafka_sarama"
+  "protocol/ws"
+  "observability/opencensus"
 )
 
 for i in "${MODULES[@]}"; do
