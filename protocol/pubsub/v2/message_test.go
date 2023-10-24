be31358463a2e5dc846ed03ed0f5df12624766cc
cpanato <ctadeu@gmail.com>
2023-10-24 16:01:05 +0200

    returning the error when doing a nack in the message
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- protocol/pubsub/v2/message_test.go
+++ protocol/pubsub/v2/message_test.go
@@ -7,6 +7,7 @@ package pubsub
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"cloud.google.com/go/pubsub"
@@ -41,6 +42,45 @@ func TestReadStructured(t *testing.T) {
 			err := msg.ReadStructured(context.Background(), (*pubsubMessagePublisher)(tc.pm))
 			if err != tc.wantErr {
 				t.Errorf("Error unexpected. got: %v, want: %v", err, tc.wantErr)
+			}
+		})
+	}
+}
+
+func TestFinish(t *testing.T) {
+	tests := []struct {
+		name    string
+		pm      *pubsub.Message
+		err     error
+		wantErr bool
+	}{
+		{
+			name: "return error",
+			pm: &pubsub.Message{
+				ID: "testid",
+			},
+			err:     fmt.Errorf("error"),
+			wantErr: true,
+		},
+		{
+			name: "no errors",
+			pm: &pubsub.Message{
+				ID: "testid",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			msg := NewMessage(tc.pm)
+			err := msg.Finish(tc.err)
+			if tc.wantErr {
+				if err != tc.err {
+					t.Errorf("Error mismatch. got: %v, want: %v", err, tc.err)
+				}
+			}
+			if !tc.wantErr && err != nil {
+				t.Errorf("Should not error but got: %v", err)
 			}
 		})
 	}
