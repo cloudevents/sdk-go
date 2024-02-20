9c287f1ac1009151354913c36c3ac58796b45ec3
Matthias Wessendorf <mwessend@redhat.com>
2024-02-20 08:41:18 +0100

    Adding a simple dataref extension, similar to the java sdk
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    Signed-off-by: Doug Davis <duglin@gmail.com>

---
+++ v2/extensions/dataref_extension.go
@@ -0,0 +1,36 @@
+/*
+ Copyright 2024 The CloudEvents Authors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package extensions
+
+import (
+	"github.com/cloudevents/sdk-go/v2/event"
+	"net/url"
+)
+
+const DataRefExtensionKey = "dataref"
+
+type DataRefExtension struct {
+	DataRef string `json:"dataref"`
+}
+
+func AddDataRefExtension(e *event.Event, dataRef string) error {
+	if _, err := url.Parse(dataRef); err != nil {
+		return err
+	}
+	e.SetExtension(DataRefExtensionKey, dataRef)
+	return nil
+}
+
+func GetDataRefExtension(e event.Event) (*DataRefExtension, bool) {
+	if dataRefValue, ok := e.Extensions()[DataRefExtensionKey]; ok {
+		dataRefStr, ok := dataRefValue.(string)
+		if !ok {
+			return nil, false
+		}
+		return &DataRefExtension{DataRef: dataRefStr}, true
+	}
+	return nil, false
+}
