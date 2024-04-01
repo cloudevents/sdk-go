bd8149ebb3cb9451fe15756f21cdaf31bc5772d5
Meng Yan <myan@redhat.com>
2024-04-01 03:36:12 +0000

    Add eventHandler option for confluent kafka producer
    Signed-off-by: Meng Yan <myan@redhat.com>
    
    reply review
    
    Signed-off-by: Meng Yan <myan@redhat.com>
    
    Update protocol/kafka_confluent/v2/protocol.go
    
    Co-authored-by: Michael Gasch <15986659+embano1@users.noreply.github.com>
    Signed-off-by: Meng Yan <myan@redhat.com>
    Signed-off-by: Noah Kreiger <noahkreiger@gmail.com>

--- samples/kafka_confluent/sender/main.go
+++ samples/kafka_confluent/sender/main.go
@@ -8,6 +8,7 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 
 	confluent "github.com/cloudevents/sdk-go/protocol/kafka_confluent/v2"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
@@ -26,8 +27,49 @@ func main() {
 	sender, err := confluent.New(confluent.WithConfigMap(&kafka.ConfigMap{
 		"bootstrap.servers": "127.0.0.1:9092",
 	}), confluent.WithSenderTopic(topic))
+	if err != nil {
+		log.Fatalf("failed to create protocol, %v", err)
+	}
 
-	defer sender.Close(ctx)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Listen to all the events on the default events channel
+	// It's important to read these events otherwise the events channel will eventually fill up
+	go func() {
+		defer wg.Done()
+		eventChan, err := sender.Events()
+		if err != nil {
+			log.Fatalf("failed to get events channel for sender, %v", err)
+		}
+		for e := range eventChan {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				// The message delivery report, indicating success or
+				// permanent failure after retries have been exhausted.
+				// Application level retries won't help since the client
+				// is already configured to do that.
+				m := ev
+				if m.TopicPartition.Error != nil {
+					log.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+				} else {
+					log.Printf("Delivered message to topic %s [%d] at offset %v\n",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+				}
+			case kafka.Error:
+				// Generic client instance-level errors, such as
+				// broker connection failures, authentication issues, etc.
+				//
+				// These errors should generally be considered informational
+				// as the underlying client will automatically try to
+				// recover from any errors encountered, the application
+				// does not need to take action on them.
+				log.Printf("Error: %v\n", ev)
+			default:
+				log.Printf("Ignored event: %v\n", ev)
+			}
+		}
+	}()
 
 	c, err := cloudevents.NewClient(sender, cloudevents.WithTimeNow(), cloudevents.WithUUIDs())
 	if err != nil {
@@ -43,14 +85,13 @@ func main() {
 			"message": "Hello, World!",
 		})
 
-		if result := c.Send(
-			// Set the producer message key
-			confluent.WithMessageKey(ctx, e.ID()),
-			e,
-		); cloudevents.IsUndelivered(result) {
+		if result := c.Send(confluent.WithMessageKey(ctx, e.ID()), e); cloudevents.IsUndelivered(result) {
 			log.Printf("failed to send: %v", result)
 		} else {
 			log.Printf("sent: %d, accepted: %t", i, cloudevents.IsACK(result))
 		}
 	}
+
+	sender.Close(ctx)
+	wg.Wait()
 }
